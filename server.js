8a5538c80e600acca7afba6679bb3c0d05fa4d3e
Joseph Young <joseph.young32@yahoo.com>
2020-01-15 07:39:55 -0500

    added Boilerplate React code and installed concurrently

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+//Require in dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+
+//Set up express
 const app = express();
 
+//Boilerplate route to test server
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
+
+//Connecting to MongoDB with mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/react-mongo-scraper",
+  {
+    useMongoClient: true
+  }
+);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
