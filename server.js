198a87681d235abc09129b9e6e485c77c8c2870a
Joseph Young <joseph.young32@yahoo.com>
2020-01-27 18:46:54 -0500

    redid server file to run express and connect to mongo with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Require in dependencies
 const express = require('express');
+const mongoose = require("mongoose");
 const logger = require('morgan');
+const routes = require("./routes");
 
 //Set up express
 const app = express();
@@ -11,8 +13,38 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Routes
-require('./routes')(app)
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
+
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/react-mongo-scraper",
+  {
+    useMongoClient: true
+  }
+);
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function (error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function () {
+  console.log("Mongoose connection successful.");
+});
+
+
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, console.log(`App is running on localhost:${PORT}`));
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
