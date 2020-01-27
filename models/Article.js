27afa8a0253f1bcb4e27bc6612681a35e4c4f310
Joseph Young <joseph.young32@yahoo.com>
2020-01-26 21:19:55 -0500

    edited routes

--- models/Article.js
+++ models/Article.js
@@ -1,42 +1,38 @@
+// Require mongoose
 var mongoose = require("mongoose");
-
-// Save a reference to the Schema constructor
+// Create Schema class
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
+// Create article schema
 var ArticleSchema = new Schema({
-  // `headline` is required and of type String
-  headline: {
-    type: String,
-    required: true
-  },
-  // `url` is required and of type String
-  url: {
-    type: String,
-    required: true
-  },
-  // `summary` is required and of type String
-  summary: {
-    type: String,
-    required: true
-  },
-  // `save` is required and of type String
-  saved: {
-    type: Boolean,
-    default: false
-  },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: [{
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }]
+    // title is a required string
+    title: {
+        type: String,
+        required: true
+    },
+    // link is a required string
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: false
+    },
+    // issaved: {
+    //     type: Boolean,
+    //     default: false,
+    //     required: true
+    // },
+    // This only saves one note's ObjectId, ref refers to the Note model
+    notes: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }]
 });
 
-// This creates our model from the above schema, using mongoose's model method
+// Create the Article model with the ArticleSchema
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
+// Export the model
 module.exports = Article;
---
+++ routes/api/articles.js
@@ -0,0 +1,243 @@
+var path = require('path');
+var request = require("request");
+var express = require("express");
+var app = express.Router();
+
+
+//scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+
+
+//models 
+var Note = require("../../models/Note.js");
+var Article = require("../../models/Article.js");
+
+const router = require("express").Router();
+
+
+// Routes
+//==========================
+
+//Notes Routes BEGIN ---------------------------------------------------------------
+
+//DELETE A NOTE
+app.delete("/deleteNote/:id", function (req, res) {
+    Note.findByIdAndRemove(req.params.id, function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        else {
+            // Or send the document to the browser
+            res.send(doc);
+        }
+    });
+
+});
+
+
+//DELETE AN ARTICLE
+app.delete("/deleteArticle/:id", function (req, res) {
+    Article.findByIdAndRemove(req.params.id, function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        else {
+            // Or send the document to the browser
+            res.send(doc);
+        }
+    });
+
+});
+
+//SAVE A NOTE
+app.post("/saveNote", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    let result = {
+        title: req.body.noteText,
+        noteText: req.body.noteText
+    }
+    var newNote = new Note(result);
+
+    // And save the new note the db
+    newNote.save(function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Otherwise
+        else {
+            // Use the article id to find and update it's note
+            Article.findOneAndUpdate({ "_id": req.body._id }, { $push: { "notes": doc._id } },
+                { safe: true, upsert: true })
+                // Execute the above query
+                .exec(function (err, doc) {
+                    // Log any errors
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        // Or send the document to the browser
+                        res.send(doc);
+                    }
+                });
+        }
+    });
+
+});
+
+
+//SEARCH NOTES BY ARTICLE ID
+app.get("/getNotes/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    article = req.body
+    Article.findOne({ "_id": req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("notes")
+        // now, execute our query
+        .exec(function (error, doc) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            }
+            // Otherwise, send the doc to the browser as a json object
+            else {
+                res.json(doc);
+            }
+        });
+});
+
+//NOTES END -----------------------------------------------------------
+
+
+//ARTICLE ROUTES BEGIN -----------------------------------------------------
+
+
+//RENDER SAVED ARTICLES
+app.get("/renderSavedArticles", function (req, res) {
+    Article.find({}, function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Or send the doc to the browser as a json object
+        else {
+            res.json(doc);
+        }
+    });
+});
+
+//SAVE ARTICLE FOR WHEN USER CLICKS SAVE
+app.post("/saveArticle", function (req, res) {
+    var result = req.body;
+    // Using our Article model, create a new entry
+    // This effectively passes the result object to the entry (and the title and link)
+    var entry = new Article(result);
+
+    // Now, save that entry to the db
+    entry.save(function (err, doc) {
+        // Log any errors
+        if (err) {
+            console.log(err);
+        }
+        // Or log the doc
+        else {
+            console.log(doc);
+        }
+    });
+
+});
+
+//GET ALL THE BLOOMBERG HEADLINES (NOT IN USE) 
+app.get("/bloomberg", function(req, res) {
+    console.log("I'm getting bloomberg headlines");
+    // First, we grab the body of the html with request
+    request("http://www.bloomberg.com", function (error, response, html) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(html);
+            // Now, we grab every h2 within an article tag, and do the following:
+            // Save an empty result object
+            var result = {
+                articles: []
+            };
+            $("article").each(function (i, element) {
+
+                var article = {
+
+                }
+                // Add the text and href of every link, and save them as properties of the result object
+                article.title = $(this).children("h2").text();
+                article.link = $(this).children("h2").children("a").attr("href");
+                article.summary = $(this).children(".summary").text();
+                // article.issaved = false;
+                article.id = i;
+                if (article.title && article.link && article.summary) {
+                    result.articles.push(article);
+                }
+
+            });
+
+
+            res.json(result);
+
+
+            //end of request
+        }
+    })
+});
+
+//GET ALL THE HEADLINES From the Nytimes
+app.get("/getHeadlines", function (req, res) {
+    // First, we grab the body of the html with request
+    request("https://www.nytimes.com/", function (error, response, html) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(html);
+            // Now, we grab every h2 within an article tag, and do the following:
+            // Save an empty result object
+            var result = {
+                articles: []
+            };
+
+            $("article").each(function (i, element) {
+                
+                var article = {
+
+                }
+                // Add the text and href of every link, and save them as properties of the result object
+                article.title = $(this).find("h2").text();
+                article.link = "https://www.nytimes.com" + $(this).find("a").attr("href");
+                article.summary = $(this).find("p").text();
+                article.isSaved = false;
+                article.id = i;
+
+                if(article.title && article.link && article.summary) {
+                    result.articles.push(article);
+                }
+
+            });
+
+
+            res.json(result);
+
+
+            //end of request
+        } 
+    })
+
+});
+
+//Articles END ------------------------------------------------------
+
+
+
+
+
+module.exports = app;
