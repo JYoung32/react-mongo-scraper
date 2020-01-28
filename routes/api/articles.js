8c723a8d0ec99ae29a02077d14ef402b40b437b6
Joseph Young <joseph.young32@yahoo.com>
2020-01-27 19:56:44 -0500

    got articles to render to react frontend, functionality working, styling remains

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -5,15 +5,12 @@ var app = express.Router();
 
 
 //scraping tools
-var request = require("request");
 var cheerio = require("cheerio");
 
 
 //models 
 var Note = require("../../models/Note.js");
 var Article = require("../../models/Article.js");
-
-const router = require("express").Router();
 
 
 // Routes
